870709c7edd3a133230777d7b79ce31724c4bcb6
atkmehmet <durmazmehmet47@gmail.com>
2025-10-11 23:42:34 +0300

    Repositort implemantation continue

---
+++ app/src/main/java/com/example/jetsat/data/local/repository/InvoiceRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package com.example.jetsat.data.local.repository
+
+import com.example.jetsat.data.local.dao.InvoiceTypeDao
+import com.example.jetsat.domain.model.Invoice
+import com.example.jetsat.domain.repository.InvoiceRepository
+import kotlinx.coroutines.flow.Flow
+
+class InvoiceRepositoryImpl(
+    private val invoiceTypeDao: InvoiceTypeDao
+):InvoiceRepository {
+    override suspend fun saveUpdateInvoice(invoice: Invoice) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteInvoice(invoiceId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getInvoice(): Flow<List<Invoice>> {
+        TODO("Not yet implemented")
+    }
+}
