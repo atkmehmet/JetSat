504c36472e1a6261dcf681d8e3a0b16d86e00ebe
atkmehmet <durmazmehmet47@gmail.com>
2025-10-20 22:08:43 +0300

    starting UI

--- app/src/main/java/com/example/jetsat/domain/model/InvoiceType.kt
+++ app/src/main/java/com/example/jetsat/domain/model/InvoiceType.kt
@@ -4,8 +4,8 @@ import androidx.room.PrimaryKey
 
 data class InvoiceType(
     val id: Int = 0,
-    val code: String,
-    val name: String,
+    val code: String = "",
+    val name: String = "",
     val description: String? = null
 ){
     fun isValid():Boolean{
---
+++ app/src/main/java/com/example/jetsat/repsentation/ViewModel/InvoiceTypeViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.jetsat.repsentation.ViewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetsat.domain.model.InvoiceType
+import com.example.jetsat.domain.repository.InvoiceTypeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class InvoiceTypeViewModel @Inject constructor(
+    private val invoiceTypeRepository: InvoiceTypeRepository
+):ViewModel() {
+
+    private val _invoiceTypeList = MutableStateFlow<List<InvoiceType>>(emptyList())
+     val invoiceTypeList :StateFlow<List<InvoiceType>> = _invoiceTypeList
+
+    var invoiceType by mutableStateOf(InvoiceType())
+
+    init {
+        viewModelScope.launch {
+            invoiceTypeRepository.getInvoiceType().map {
+                _invoiceTypeList.value = it
+            }
+        }
+    }
+
+    fun saveUpdate(){
+        viewModelScope.launch {
+            invoiceTypeRepository.saveUpdateInvoiceType(invoiceType)
+        }
+    }
+
+    fun delete(id:Int){
+        viewModelScope.launch {
+            invoiceTypeRepository.deleteInvoiceType(id)
+        }
+    }
+}
