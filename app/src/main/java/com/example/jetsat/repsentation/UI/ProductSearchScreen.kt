09d709f9fa3b44af2d021f9cd8cdc637e9426712
atkmehmet <durmazmehmet47@gmail.com>
2025-11-17 23:03:53 +0300

    error found and solved

--- app/src/main/java/com/example/jetsat/repsentation/UI/ProductSearchScreen.kt
+++ app/src/main/java/com/example/jetsat/repsentation/UI/ProductSearchScreen.kt
@@ -39,7 +39,7 @@ fun ProductSearchScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Button(onClick = { viewModel.openScanner() }) {
-                Text("📷 Barkod Oku")
+                Text("📷 Barkod Read")
             }
 
             Spacer(modifier = Modifier.height(20.dp))
