f9896ce4262f956a4766123ecc7932c8dff82849
atkmehmet <durmazmehmet47@gmail.com>
2025-11-23 23:33:54 +0300

    productScreen add LazyColumn

--- app/src/main/java/com/example/jetsat/repsentation/UI/ProductScreen.kt
+++ app/src/main/java/com/example/jetsat/repsentation/UI/ProductScreen.kt
@@ -1,14 +1,26 @@
 package com.example.jetsat.repsentation.UI
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -17,9 +29,13 @@ import com.example.jetsat.repsentation.ViewModel.ProductViewModel
 import com.example.jetsat.repsentation.components.AppOutlinedTextField
 import com.example.jetsat.repsentation.components.CustomOutlinedTextField
 import com.example.jetsat.repsentation.components.SearchableDropdownTextField
+import androidx.compose.foundation.lazy.items
 
 @Composable
 fun ProductScreen( productViewModel: ProductViewModel = hiltViewModel()){
+
+    val productList by productViewModel.productList.collectAsState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -84,6 +100,72 @@ fun ProductScreen( productViewModel: ProductViewModel = hiltViewModel()){
             label = "Choose Category",
             placeholder = "Choose Category"
         )
+
+        Spacer(Modifier.height(16.dp))
+
+        // -----------------------------
+        //      LISTVIEW (LazyColumn)
+        // -----------------------------
+        Text(
+            text = "Product List",
+            style = MaterialTheme.typography.titleMedium
+        )
+
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+        ) {
+
+            items( items = productList,
+                key = { it.id }  ) { item ->
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp),
+                    elevation = CardDefaults.cardElevation(4.dp)
+                ) {
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(12.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+
+                        // Ürün Bilgileri
+                        Column(modifier = Modifier.weight(1f)) {
+                            Text(text = "Name: ${item.productName}")
+                            Text(text = "Buy: ${item.productTakePrice} TL")
+                            Text(text = "Sell: ${item.productSoldPrice} TL")
+                        }
+
+                        // Düzenle & Sil Butonları
+                        Row {
+
+                            // DÜZENLE BUTONU
+                            IconButton(onClick = { productViewModel.onEditProduct(item) }) {
+                                Icon(
+                                    imageVector = Icons.Default.Edit,
+                                    contentDescription = "Edit"
+                                )
+                            }
+
+                            // SİL BUTONU
+                            IconButton(onClick = { productViewModel.deleteProduct(item) }) {
+                                Icon(
+                                    imageVector = Icons.Default.Delete,
+                                    contentDescription = "Delete"
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
 
     }
 }
