b1dec3077508f38f155a3f51d677d70e3e2932c6
atkmehmet <durmazmehmet47@gmail.com>
2025-12-12 23:45:03 +0300

    design change contunie

--- app/src/main/java/com/example/jetsat/repsentation/components/Accordion.kt
+++ app/src/main/java/com/example/jetsat/repsentation/components/Accordion.kt
@@ -1,14 +1,21 @@
 package com.example.jetsat.repsentation.components
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Accordion(){
@@ -17,9 +24,14 @@ fun Accordion(){
         mutableStateOf(false)
     }
 
-    Column(modifier = Modifier.fillMaxSize()
-        .padding(16.dp),
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
         ) {
+
+        Row ( modifier = Modifier.fillMaxWidth()){
+ 
+        }
 
     }
 }
