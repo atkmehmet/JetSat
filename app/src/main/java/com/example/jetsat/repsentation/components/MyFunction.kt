2041695b57bbf4777781640eca6bfc65ff6a7733
atkmehmet <durmazmehmet47@gmail.com>
2025-12-15 01:16:49 +0300

    design change contunie

--- app/src/main/java/com/example/jetsat/repsentation/components/MyFunction.kt
+++ app/src/main/java/com/example/jetsat/repsentation/components/MyFunction.kt
@@ -1,6 +1,5 @@
 package com.example.jetsat.repsentation.components
 
-import androidx.collection.emptyLongSet
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateFloatAsState
@@ -13,24 +12,23 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableFloatState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
@@ -40,68 +38,56 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun Accordion() {
-
-    var openClose by remember { mutableStateOf(false) }
+fun MyFunction(){
+    var openClose by remember {
+        mutableStateOf(false)
+    }
 
     val rotation by animateFloatAsState(
-        targetValue = if (openClose) 180f else 0f,
-        label = ""
-    )
+        targetValue = if(openClose) 180f else 0f,
+        label = "")
+
 
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 40.dp)
+            .padding(vertical = 20.dp)
             .animateContentSize(),
         shape = RoundedCornerShape(12.dp),
         elevation = CardDefaults.cardElevation(4.dp)
-    ) {
 
-        Column (
-            modifier = Modifier.background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(
-                        Color(0xFF2196F3), // Mavi
-                        Color(0xFF21CBF3)  // Açık mavi
-                    )
+    ) {
+        Column(modifier = Modifier.background(
+            brush = Brush.verticalGradient(
+                colors = listOf(
+                    Color(0xFF2196F3), // Mavi
+                    Color(0xFF21CBF3)  // Açık mavi
                 )
             )
-        ){
-            Row(
-                modifier = Modifier
+        )) {
+                Row (modifier = Modifier
                     .fillMaxWidth()
                     .clickable { openClose = !openClose }
-                    .padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "See Products",
-                    modifier = Modifier.weight(1f),
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Medium
+                    .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 )
-
-                Icon(
-                    imageVector = Icons.Default.KeyboardArrowDown,
-                    contentDescription = "",
-                    modifier = Modifier.rotate(rotation)
-                )
-            }
-
-            AnimatedVisibility(
-                visible = openClose,
-                enter = fadeIn() + expandVertically(),
-                exit = fadeOut() + shrinkVertically()
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    Text("Product 1")
-                    Text("Product 2")
-                    Text("Product 3")
+                {
+                   
+                    Text(text = "See Products"
+                    , modifier = Modifier.weight(1f)
+                    , fontSize = 16.sp
+                    , fontWeight = FontWeight.Medium)
+    
+                    Icon(imageVector = Icons.Default.KeyboardArrowDown, contentDescription =""
+                        , modifier = Modifier.rotate(rotation))
                 }
+            AnimatedVisibility(visible = openClose,
+                enter = fadeIn()+ expandVertically () ,
+                exit = fadeOut() + shrinkVertically ()
+            ) {
+                Text(text = "Hello World")
             }
-        }
+
+           }
     }
 }
